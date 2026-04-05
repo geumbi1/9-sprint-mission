6459909710fa143e075fe0d89539b1dabd12e168
geumbi <gkrry0404@naver.com>
2026-04-05 19:53:24 +0900

    새 브랜치파서 다시 올림

--- discodeit/src/test/java/com/sprint/mission/discodeit/controller/UserControllerTest.java
+++ discodeit/src/test/java/com/sprint/mission/discodeit/controller/UserControllerTest.java
@@ -27,6 +27,7 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.servlet.View;
 
+//..
 @WebMvcTest(UserController.class)
 class UserControllerTest {
 
@@ -38,7 +39,7 @@ class UserControllerTest {
 
   @MockBean
   private UserStatusService userStatusService;
-  
+
   @MockBean
   private View error;
 
